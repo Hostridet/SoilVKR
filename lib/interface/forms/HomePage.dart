e0fb9fe7a967ce7c4a54ebd591f19a9f7d6a6d13
nrumiantcev <rumiantcev.no@students.dvfu.ru>
2023-04-26 18:52:28 +1000

    добавил рабочую карту

--- lib/interface/forms/HomePage.dart
+++ lib/interface/forms/HomePage.dart
@@ -1,9 +1,8 @@
 import 'dart:async';
-import 'package:map_picker/map_picker.dart';
 import 'package:new_gradient_app_bar/new_gradient_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:soil/interface/components/Drawer.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location_picker_flutter_map/location_picker_flutter_map.dart';
 import 'package:geocoding/geocoding.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,13 +13,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final _controller = Completer<GoogleMapController>();
-  MapPickerController mapPickerController = MapPickerController();
-  CameraPosition cameraPosition = const CameraPosition(
-    target: LatLng(41.311158, 69.279737),
-    zoom: 14.4746,
-  );
-  var textController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,91 +24,23 @@ class _HomePageState extends State<HomePage> {
       body: Stack(
         alignment: Alignment.topCenter,
         children: [
-          MapPicker(
-            // pass icon widget
-            //add map picker controller
-            mapPickerController: mapPickerController,
-            child: GoogleMap(
-              myLocationEnabled: true,
-              zoomControlsEnabled: false,
-              // hide location button
-              myLocationButtonEnabled: false,
-              mapType: MapType.normal,
-              //  camera position
-              initialCameraPosition: cameraPosition,
-              onMapCreated: (GoogleMapController controller) {
-                _controller.complete(controller);
-              },
-              onCameraMoveStarted: () {
-                // notify map is moving
-                mapPickerController.mapMoving!();
-                textController.text = "checking ...";
-              },
-              onCameraMove: (cameraPosition) {
-                this.cameraPosition = cameraPosition;
-              },
-              onCameraIdle: () async {
-                // notify map stopped moving
-                mapPickerController.mapFinishedMoving!();
-                //get address name from camera position
-                List<Placemark> placemarks = await placemarkFromCoordinates(
-                  cameraPosition.target.latitude,
-                  cameraPosition.target.longitude,
-                );
-
-                // update the ui with the address
-                textController.text =
-                '${placemarks.first.name}, ${placemarks.first.administrativeArea}, ${placemarks.first.country}';
-              },
-            ),
-          ),
-          Positioned(
-            top: MediaQuery.of(context).viewPadding.top + 20,
-            width: MediaQuery.of(context).size.width - 50,
-            height: 50,
-            child: TextFormField(
-              maxLines: 3,
-              textAlign: TextAlign.center,
-              readOnly: true,
-              decoration: const InputDecoration(
-                  contentPadding: EdgeInsets.zero, border: InputBorder.none),
-              controller: textController,
-            ),
-          ),
-          Positioned(
-            bottom: 24,
-            left: 24,
-            right: 24,
-            child: SizedBox(
-              height: 50,
-              child: TextButton(
-                child: const Text(
-                  "Submit",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w400,
-                    fontStyle: FontStyle.normal,
-                    color: Color(0xFFFFFFFF),
-                    fontSize: 19,
-                    // height: 19/19,
-                  ),
-                ),
-                onPressed: () {
-                  print(
-                      "Location ${cameraPosition.target.latitude} ${cameraPosition.target.longitude}");
-                  print("Address: ${textController.text}");
-                },
-                style: ButtonStyle(
-                  backgroundColor:
-                  MaterialStateProperty.all<Color>(const Color(0xFF32CD32)),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                  ),
-                ),
+          FlutterLocationPicker(
+              initPosition: LatLong(23, 89),
+              selectLocationButtonStyle: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.blue),
               ),
-            ),
-          )
+              selectLocationButtonText: 'Set Current Location',
+              initZoom: 11,
+              minZoomLevel: 5,
+              maxZoomLevel: 16,
+              trackMyPosition: true,
+              onError: (e) => print(e),
+              onPicked: (pickedData) {
+                print(pickedData.latLong.latitude);
+                print(pickedData.latLong.longitude);
+                print(pickedData.address);
+                print(pickedData.addressData['country']);
+              })
         ],
       ),
     );
