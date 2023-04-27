769fa53869a2badde415f1e62cc961f6503c39d8
Nikita Rumiantsev <nilita.rumyan@gmail.com>
2023-04-27 20:33:02 +1000

    обновил карту

--- lib/interface/forms/HomePage.dart
+++ lib/interface/forms/HomePage.dart
@@ -3,10 +3,8 @@ import 'package:new_gradient_app_bar/new_gradient_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:soil/interface/components/Drawer.dart';
 import 'package:location_picker_flutter_map/location_picker_flutter_map.dart';
-import 'package:geocoding/geocoding.dart';
-
-import '../components/InfoLayout.dart';
-
+import 'package:flutter/foundation.dart';
+import 'package:soil/interface/components/InfoLayout.dart';
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -23,32 +21,28 @@ class _HomePageState extends State<HomePage> {
         title: const Text('Главный экран'),
         gradient: const LinearGradient(colors: [Color(0xff228B22), Color(0xff008000), Color(0xff006400)]),
       ),
-      body: Stack(
-        alignment: Alignment.topCenter,
-        children: [
-          FlutterLocationPicker(
-              mapLanguage: "ru",
-              initPosition: LatLong(23, 89),
-              selectLocationButtonStyle: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.green),
-              ),
-              selectLocationButtonText: 'Выбрать',
-              initZoom: 11,
-              minZoomLevel: 5,
-              maxZoomLevel: 16,
-              trackMyPosition:  false,
-              searchBarBackgroundColor: Colors.white,
-              onError: (e) {
-                InfoLayout.buildErrorLayout(context, e.toString());
-              },
-              onPicked: (pickedData) {
-                print(pickedData.latLong.latitude);
-                print(pickedData.latLong.longitude);
-                print(pickedData.address);
-                print(pickedData.addressData['country']);
-              })
-        ],
-      ),
+      body: FlutterLocationPicker(
+          initPosition: LatLong(43.10562, 131.87353),
+          showSearchBar: false,
+          searchBarBackgroundColor: Colors.white,
+          mapLanguage: 'ru',
+          selectLocationButtonText: 'Выбрать',
+          selectLocationButtonStyle: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.green),
+          ),
+          initZoom: 11,
+          minZoomLevel: 5,
+          maxZoomLevel: 16,
+          trackMyPosition: false,
+          onError: (e) {
+            InfoLayout.buildErrorLayout(context, e.toString());
+          },
+          onPicked: (pickedData) {
+            print(pickedData.latLong.latitude);
+            print(pickedData.latLong.longitude);
+            print(pickedData.address);
+            print(pickedData.addressData['country']);
+          })
     );
   }
 }
